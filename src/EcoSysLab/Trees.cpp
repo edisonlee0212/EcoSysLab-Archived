2ac1f2f64a8cdf697460861124e811d0f1a441e7
Bosheng Li <li2343@purdue.edu>
2022-10-27 02:26:36 -0400

    Update Trees.cpp

--- src/EcoSysLab/Trees.cpp
+++ src/EcoSysLab/Trees.cpp
@@ -53,141 +53,146 @@ void Trees::OnInspect() {
         }
 
     }
-    static float lastUsedTime = 0.0f;
-    if (ImGui::Button("Grow")) {
-        float time = Application::Time().CurrentTime();
-        std::vector<std::shared_future<void>> results;
-        Jobs::ParallelFor(m_trees.size(), [&](unsigned i) {
-            auto descriptor = m_trees[i].m_treeDescriptor.Get<TreeDescriptor>();
-            if(descriptor) {
-                m_trees[i].m_treeModel.Grow({999}, descriptor->m_treeStructuralGrowthParameters);
-            }
-        }, results);
-        for (auto &i: results) i.wait();
-        lastUsedTime = Application::Time().CurrentTime() - time;
-        totalTime += lastUsedTime;
-    }
-    ImGui::Text("Growth time: %.4f", lastUsedTime);
-    ImGui::Text("Total time: %.4f", totalTime);
-    ImGui::Checkbox("Visualization", &debugVisualization);
-
-
-    ImGui::Text("Internode count: %d", internodeSize);
-    ImGui::Text("Branch count: %d", branchSize);
-    if (debugVisualization && !m_trees.empty()) {
-        static bool branchOnly = false;
-        ImGui::Checkbox("Branch only", &branchOnly);
-        static std::vector<glm::mat4> matrices;
-        static std::vector<glm::vec4> colors;
-        static std::vector<glm::mat4> branchMatrices;
-        static std::vector<glm::vec4> branchColors;
-        static Handle handle;
-        bool needUpdate = handle == GetHandle();
-        if (ImGui::Button("Update")) needUpdate = true;
-        int totalInternodeSize = 0;
-        int totalBranchSize = 0;
-        for (int i = 0; i < m_trees.size(); i++) {
-            auto &tree = m_trees[i];
-            if (versions[i] != tree.m_treeModel.m_tree->GetVersion()) {
-                versions[i] = tree.m_treeModel.m_tree->GetVersion();
-                needUpdate = true;
-                sortedInternodeLists[i] = tree.m_treeModel.m_tree->GetSortedInternodeList();
-                sortedBranchLists[i] = tree.m_treeModel.m_tree->GetSortedBranchList();
-            }
-            totalInternodeSize += sortedInternodeLists[i].size();
-            totalBranchSize += sortedBranchLists[i].size();
+    if(!m_trees.empty()) {
+        static float lastUsedTime = 0.0f;
+        if (ImGui::Button("Grow")) {
+            float time = Application::Time().CurrentTime();
+            std::vector<std::shared_future<void>> results;
+            Jobs::ParallelFor(m_trees.size(), [&](unsigned i) {
+                auto descriptor = m_trees[i].m_treeDescriptor.Get<TreeDescriptor>();
+                if (descriptor) {
+                    m_trees[i].m_treeModel.Grow({999}, descriptor->m_treeStructuralGrowthParameters);
+                }
+            }, results);
+            for (auto &i: results) i.wait();
+            lastUsedTime = Application::Time().CurrentTime() - time;
+            totalTime += lastUsedTime;
         }
-        internodeSize = totalInternodeSize;
-        branchSize = totalBranchSize;
-        if (needUpdate) {
-            matrices.resize(totalInternodeSize);
-            colors.resize(totalInternodeSize);
-            branchMatrices.resize(totalBranchSize);
-            branchColors.resize(totalBranchSize);
+        ImGui::Text("Growth time: %.4f", lastUsedTime);
+        ImGui::Text("Total time: %.4f", totalTime);
+        ImGui::Checkbox("Visualization", &debugVisualization);
 
-            int startIndex = 0;
-            auto entityGlobalTransform = GetScene()->GetDataComponent<GlobalTransform>(GetOwner());
 
-            for (int listIndex = 0; listIndex < sortedInternodeLists.size(); listIndex++) {
-                auto &list = sortedInternodeLists[listIndex];
-                std::vector<std::shared_future<void>> results;
-                GlobalTransform globalTransform;
-                globalTransform.m_value = entityGlobalTransform.m_value * m_trees[listIndex].m_transform.m_value;
-                Jobs::ParallelFor(list.size(), [&](unsigned i) {
-                    auto &internode = m_trees[listIndex].m_treeModel.m_tree->RefInternode(list[i]);
-                    auto rotation = globalTransform.GetRotation() * internode.m_globalRotation;
-                    glm::vec3 translation = (globalTransform.m_value * glm::translate(internode.m_globalPosition))[3];
-                    const auto direction = glm::normalize(rotation * glm::vec3(0, 0, -1));
-                    auto localEndPosition = internode.m_globalPosition + internode.m_length * direction;
-                    const glm::vec3 position2 = (globalTransform.m_value * glm::translate(localEndPosition))[3];
-                    rotation = glm::quatLookAt(
-                            direction, glm::vec3(direction.y, direction.z, direction.x));
-                    rotation *= glm::quat(glm::vec3(glm::radians(90.0f), 0.0f, 0.0f));
-                    const glm::mat4 rotationTransform = glm::mat4_cast(rotation);
-                    matrices[i + startIndex] =
-                            glm::translate((translation + position2) / 2.0f) *
-                            rotationTransform *
-                            glm::scale(glm::vec3(
-                                    internode.m_thickness,
-                                    glm::distance(translation, position2) / 2.0f,
-                                    internode.m_thickness));
-                    colors[i + startIndex] = randomColors[m_trees[listIndex].m_treeModel.m_tree->RefBranch(
-                            internode.m_branchHandle).m_data.m_order];
-                }, results);
-                for (auto &i: results) i.wait();
-                startIndex += list.size();
+        ImGui::Text("Internode count: %d", internodeSize);
+        ImGui::Text("Branch count: %d", branchSize);
+        if (debugVisualization && !m_trees.empty()) {
+            static bool branchOnly = false;
+            ImGui::Checkbox("Branch only", &branchOnly);
+            static std::vector<glm::mat4> matrices;
+            static std::vector<glm::vec4> colors;
+            static std::vector<glm::mat4> branchMatrices;
+            static std::vector<glm::vec4> branchColors;
+            static Handle handle;
+            bool needUpdate = handle == GetHandle();
+            if (ImGui::Button("Update")) needUpdate = true;
+            int totalInternodeSize = 0;
+            int totalBranchSize = 0;
+            for (int i = 0; i < m_trees.size(); i++) {
+                auto &tree = m_trees[i];
+                if (versions[i] != tree.m_treeModel.m_tree->GetVersion()) {
+                    versions[i] = tree.m_treeModel.m_tree->GetVersion();
+                    needUpdate = true;
+                    sortedInternodeLists[i] = tree.m_treeModel.m_tree->GetSortedInternodeList();
+                    sortedBranchLists[i] = tree.m_treeModel.m_tree->GetSortedBranchList();
+                }
+                totalInternodeSize += sortedInternodeLists[i].size();
+                totalBranchSize += sortedBranchLists[i].size();
             }
+            internodeSize = totalInternodeSize;
+            branchSize = totalBranchSize;
+            if (needUpdate) {
+                matrices.resize(totalInternodeSize);
+                colors.resize(totalInternodeSize);
+                branchMatrices.resize(totalBranchSize);
+                branchColors.resize(totalBranchSize);
 
-            startIndex = 0;
-            for (int listIndex = 0; listIndex < sortedBranchLists.size(); listIndex++) {
-                auto &list = sortedBranchLists[listIndex];
-                std::vector<std::shared_future<void>> results;
-                GlobalTransform globalTransform;
-                globalTransform.m_value = entityGlobalTransform.m_value * m_trees[listIndex].m_transform.m_value;
-                Jobs::ParallelFor(list.size(), [&](unsigned i) {
-                    auto &branch = m_trees[listIndex].m_treeModel.m_tree->RefBranch(list[i]);
-                    glm::vec3 translation = (globalTransform.m_value * glm::translate(branch.m_globalStartPosition))[3];
-                    const auto direction = glm::normalize(branch.m_globalEndPosition - branch.m_globalStartPosition);
-                    auto length = glm::distance(branch.m_globalStartPosition, branch.m_globalEndPosition);
-                    auto thickness = (branch.m_startThickness + branch.m_endThickness) * 0.5;
-                    const glm::vec3 position2 = (globalTransform.m_value *
-                                                 glm::translate(branch.m_globalEndPosition))[3];
-                    auto rotation = glm::quatLookAt(
-                            direction, glm::vec3(direction.y, direction.z, direction.x));
-                    rotation *= glm::quat(glm::vec3(glm::radians(90.0f), 0.0f, 0.0f));
-                    const glm::mat4 rotationTransform = glm::mat4_cast(rotation);
-                    branchMatrices[i + startIndex] =
-                            glm::translate((translation + position2) / 2.0f) *
-                            rotationTransform *
-                            glm::scale(glm::vec3(
-                                    thickness,
-                                    glm::distance(translation, position2) / 2.0f,
-                                    thickness));
-                    branchColors[i + startIndex] = randomColors[branch.m_data.m_order];
-                }, results);
-                for (auto &i: results) i.wait();
-                startIndex += list.size();
+                int startIndex = 0;
+                auto entityGlobalTransform = GetScene()->GetDataComponent<GlobalTransform>(GetOwner());
+
+                for (int listIndex = 0; listIndex < sortedInternodeLists.size(); listIndex++) {
+                    auto &list = sortedInternodeLists[listIndex];
+                    std::vector<std::shared_future<void>> results;
+                    GlobalTransform globalTransform;
+                    globalTransform.m_value = entityGlobalTransform.m_value * m_trees[listIndex].m_transform.m_value;
+                    Jobs::ParallelFor(list.size(), [&](unsigned i) {
+                        auto &internode = m_trees[listIndex].m_treeModel.m_tree->RefInternode(list[i]);
+                        auto rotation = globalTransform.GetRotation() * internode.m_globalRotation;
+                        glm::vec3 translation = (globalTransform.m_value *
+                                                 glm::translate(internode.m_globalPosition))[3];
+                        const auto direction = glm::normalize(rotation * glm::vec3(0, 0, -1));
+                        auto localEndPosition = internode.m_globalPosition + internode.m_length * direction;
+                        const glm::vec3 position2 = (globalTransform.m_value * glm::translate(localEndPosition))[3];
+                        rotation = glm::quatLookAt(
+                                direction, glm::vec3(direction.y, direction.z, direction.x));
+                        rotation *= glm::quat(glm::vec3(glm::radians(90.0f), 0.0f, 0.0f));
+                        const glm::mat4 rotationTransform = glm::mat4_cast(rotation);
+                        matrices[i + startIndex] =
+                                glm::translate((translation + position2) / 2.0f) *
+                                rotationTransform *
+                                glm::scale(glm::vec3(
+                                        internode.m_thickness,
+                                        glm::distance(translation, position2) / 2.0f,
+                                        internode.m_thickness));
+                        colors[i + startIndex] = randomColors[m_trees[listIndex].m_treeModel.m_tree->RefBranch(
+                                internode.m_branchHandle).m_data.m_order];
+                    }, results);
+                    for (auto &i: results) i.wait();
+                    startIndex += list.size();
+                }
+
+                startIndex = 0;
+                for (int listIndex = 0; listIndex < sortedBranchLists.size(); listIndex++) {
+                    auto &list = sortedBranchLists[listIndex];
+                    std::vector<std::shared_future<void>> results;
+                    GlobalTransform globalTransform;
+                    globalTransform.m_value = entityGlobalTransform.m_value * m_trees[listIndex].m_transform.m_value;
+                    Jobs::ParallelFor(list.size(), [&](unsigned i) {
+                        auto &branch = m_trees[listIndex].m_treeModel.m_tree->RefBranch(list[i]);
+                        glm::vec3 translation = (globalTransform.m_value *
+                                                 glm::translate(branch.m_globalStartPosition))[3];
+                        const auto direction = glm::normalize(
+                                branch.m_globalEndPosition - branch.m_globalStartPosition);
+                        auto length = glm::distance(branch.m_globalStartPosition, branch.m_globalEndPosition);
+                        auto thickness = (branch.m_startThickness + branch.m_endThickness) * 0.5;
+                        const glm::vec3 position2 = (globalTransform.m_value *
+                                                     glm::translate(branch.m_globalEndPosition))[3];
+                        auto rotation = glm::quatLookAt(
+                                direction, glm::vec3(direction.y, direction.z, direction.x));
+                        rotation *= glm::quat(glm::vec3(glm::radians(90.0f), 0.0f, 0.0f));
+                        const glm::mat4 rotationTransform = glm::mat4_cast(rotation);
+                        branchMatrices[i + startIndex] =
+                                glm::translate((translation + position2) / 2.0f) *
+                                rotationTransform *
+                                glm::scale(glm::vec3(
+                                        thickness,
+                                        glm::distance(translation, position2) / 2.0f,
+                                        thickness));
+                        branchColors[i + startIndex] = randomColors[branch.m_data.m_order];
+                    }, results);
+                    for (auto &i: results) i.wait();
+                    startIndex += list.size();
+                }
+
             }
-
-        }
-        if (!matrices.empty()) {
-            auto editorLayer = Application::GetLayer<EditorLayer>();
-            if (branchOnly) {
-                Gizmos::DrawGizmoMeshInstancedColored(
-                        DefaultResources::Primitives::Cube, editorLayer->m_sceneCamera,
-                        editorLayer->m_sceneCameraPosition,
-                        editorLayer->m_sceneCameraRotation,
-                        *reinterpret_cast<std::vector<glm::vec4> *>(&branchColors),
-                        *reinterpret_cast<std::vector<glm::mat4> *>(&branchMatrices),
-                        glm::mat4(1.0f), 1.0f);
-            } else {
-                Gizmos::DrawGizmoMeshInstancedColored(
-                        DefaultResources::Primitives::Cube, editorLayer->m_sceneCamera,
-                        editorLayer->m_sceneCameraPosition,
-                        editorLayer->m_sceneCameraRotation,
-                        *reinterpret_cast<std::vector<glm::vec4> *>(&colors),
-                        *reinterpret_cast<std::vector<glm::mat4> *>(&matrices),
-                        glm::mat4(1.0f), 1.0f);
+            if (!matrices.empty()) {
+                auto editorLayer = Application::GetLayer<EditorLayer>();
+                if (branchOnly) {
+                    Gizmos::DrawGizmoMeshInstancedColored(
+                            DefaultResources::Primitives::Cube, editorLayer->m_sceneCamera,
+                            editorLayer->m_sceneCameraPosition,
+                            editorLayer->m_sceneCameraRotation,
+                            *reinterpret_cast<std::vector<glm::vec4> *>(&branchColors),
+                            *reinterpret_cast<std::vector<glm::mat4> *>(&branchMatrices),
+                            glm::mat4(1.0f), 1.0f);
+                } else {
+                    Gizmos::DrawGizmoMeshInstancedColored(
+                            DefaultResources::Primitives::Cube, editorLayer->m_sceneCamera,
+                            editorLayer->m_sceneCameraPosition,
+                            editorLayer->m_sceneCameraRotation,
+                            *reinterpret_cast<std::vector<glm::vec4> *>(&colors),
+                            *reinterpret_cast<std::vector<glm::mat4> *>(&matrices),
+                            glm::mat4(1.0f), 1.0f);
+                }
             }
         }
     }
