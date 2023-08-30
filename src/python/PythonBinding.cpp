54c6dcb309f8fc7084475ba85388e22bfdbc63d5
Bosheng Li <li2343@purdue.edu>
2023-08-30 12:08:30 -0400

    Update PythonBinding.cpp

--- src/python/PythonBinding.cpp
+++ src/python/PythonBinding.cpp
@@ -180,13 +180,67 @@ PYBIND11_MODULE(pyecosyslab, m) {
 		.def("get_version", &Entity::GetVersion);
 
 	py::class_<ConnectivityGraphSettings>(m, "ConnectivityGraphSettings")
-		.def(py::init<>());
+		.def(py::init<>())
+		.def_readwrite("m_scatterPointsConnectionMaxLength", &ConnectivityGraphSettings::m_scatterPointsConnectionMaxLength)
+		.def_readwrite("m_scatterPointBranchConnectionMaxLength", &ConnectivityGraphSettings::m_scatterPointBranchConnectionMaxLength)
+		.def_readwrite("m_edgeExtendStep", &ConnectivityGraphSettings::m_edgeExtendStep)
+		.def_readwrite("m_edgeLength", &ConnectivityGraphSettings::m_edgeLength)
+		.def_readwrite("m_maxTimeout", &ConnectivityGraphSettings::m_maxTimeout)
+		.def_readwrite("m_forceConnectionAngleLimit", &ConnectivityGraphSettings::m_forceConnectionAngleLimit)
+		.def_readwrite("m_forceConnectionRatio", &ConnectivityGraphSettings::m_forceConnectionRatio)
+		.def_readwrite("m_absoluteAngleLimit", &ConnectivityGraphSettings::m_absoluteAngleLimit)
+		.def_readwrite("m_branchShortening", &ConnectivityGraphSettings::m_branchShortening);
+
+
 
 	py::class_<ReconstructionSettings>(m, "ReconstructionSettings")
-		.def(py::init<>());
+		.def(py::init<>())
+		.def_readwrite("m_internodeLength", &ReconstructionSettings::m_internodeLength)
+		.def_readwrite("m_minHeight", &ReconstructionSettings::m_minHeight)
+		.def_readwrite("m_maxTreeDistance", &ReconstructionSettings::m_maxTreeDistance)
+		.def_readwrite("m_branchShortening", &ReconstructionSettings::m_branchShortening)
+		.def_readwrite("m_minThickness", &ReconstructionSettings::m_minThickness)
+		.def_readwrite("m_overrideThickness", &ReconstructionSettings::m_overrideThickness)
+		.def_readwrite("m_minimumNodeCount", &ReconstructionSettings::m_minimumNodeCount);
+
+	py::class_<PresentationOverrideSettings>(m, "PresentationOverrideSettings")
+		.def(py::init<>())
+		.def_readwrite("m_leafCountPerInternode", &PresentationOverrideSettings::m_leafCountPerInternode)
+		.def_readwrite("m_distanceToEndLimit", &PresentationOverrideSettings::m_distanceToEndLimit)
+		.def_readwrite("m_positionVariance", &PresentationOverrideSettings::m_positionVariance)
+		.def_readwrite("m_phototropism", &PresentationOverrideSettings::m_phototropism)
+		.def_readwrite("m_limitMaxThickness", &PresentationOverrideSettings::m_limitMaxThickness);
 
 	py::class_<TreeMeshGeneratorSettings>(m, "TreeMeshGeneratorSettings")
-		.def(py::init<>());
+		.def(py::init<>())
+		.def_readwrite("m_vertexColorOnly", &TreeMeshGeneratorSettings::m_vertexColorOnly)
+		.def_readwrite("m_enableFoliage", &TreeMeshGeneratorSettings::m_enableFoliage)
+		.def_readwrite("m_enableFruit", &TreeMeshGeneratorSettings::m_enableFruit)
+		.def_readwrite("m_enableBranch", &TreeMeshGeneratorSettings::m_enableBranch)
+		.def_readwrite("m_enableRoot", &TreeMeshGeneratorSettings::m_enableRoot)
+		.def_readwrite("m_enableFineRoot", &TreeMeshGeneratorSettings::m_enableFineRoot)
+		.def_readwrite("m_enableTwig", &TreeMeshGeneratorSettings::m_enableTwig)
+		.def_readwrite("m_overridePresentation", &TreeMeshGeneratorSettings::m_overridePresentation)
+		.def_readwrite("m_presentationOverrideSettings", &TreeMeshGeneratorSettings::m_presentationOverrideSettings)
+		.def_readwrite("m_resolution", &TreeMeshGeneratorSettings::m_resolution)
+		.def_readwrite("m_subdivision", &TreeMeshGeneratorSettings::m_subdivision)
+		.def_readwrite("m_overrideRadius", &TreeMeshGeneratorSettings::m_overrideRadius)
+		.def_readwrite("m_radius", &TreeMeshGeneratorSettings::m_radius)
+		.def_readwrite("m_overrideVertexColor", &TreeMeshGeneratorSettings::m_overrideVertexColor)
+		.def_readwrite("m_markJunctions", &TreeMeshGeneratorSettings::m_markJunctions)
+		.def_readwrite("m_junctionLowerRatio", &TreeMeshGeneratorSettings::m_junctionLowerRatio)
+		.def_readwrite("m_junctionUpperRatio", &TreeMeshGeneratorSettings::m_junctionUpperRatio)
+		.def_readwrite("m_baseControlPointRatio", &TreeMeshGeneratorSettings::m_baseControlPointRatio)
+		.def_readwrite("m_branchControlPointRatio", &TreeMeshGeneratorSettings::m_branchControlPointRatio)
+		.def_readwrite("m_lineLengthFactor", &TreeMeshGeneratorSettings::m_lineLengthFactor)
+		.def_readwrite("m_smoothness", &TreeMeshGeneratorSettings::m_smoothness)
+		.def_readwrite("m_autoLevel", &TreeMeshGeneratorSettings::m_autoLevel)
+		.def_readwrite("m_voxelSubdivisionLevel", &TreeMeshGeneratorSettings::m_voxelSubdivisionLevel)
+		.def_readwrite("m_voxelSmoothIteration", &TreeMeshGeneratorSettings::m_voxelSmoothIteration)
+		.def_readwrite("m_removeDuplicate", &TreeMeshGeneratorSettings::m_removeDuplicate)
+		.def_readwrite("m_branchMeshType", &TreeMeshGeneratorSettings::m_branchMeshType)
+		.def_readwrite("m_rootMeshType", &TreeMeshGeneratorSettings::m_rootMeshType)
+		.def_readwrite("m_detailedFoliage", &TreeMeshGeneratorSettings::m_detailedFoliage);
 
 	py::class_<Scene>(m, "Scene")
 		.def("create_entity", static_cast<Entity(Scene::*)(const std::string&)>(&Scene::CreateEntity))
@@ -194,11 +248,11 @@ PYBIND11_MODULE(pyecosyslab, m) {
 
 	py::class_<ApplicationInfo>(m, "ApplicationInfo")
 		.def(py::init<>())
-		.def_readwrite("projectPath", &ApplicationInfo::m_projectPath)
-		.def_readwrite("applicationName", &ApplicationInfo::m_applicationName)
-		.def_readwrite("enableDocking", &ApplicationInfo::m_enableDocking)
-		.def_readwrite("enableViewport", &ApplicationInfo::m_enableViewport)
-		.def_readwrite("fullscreen", &ApplicationInfo::m_fullScreen);
+		.def_readwrite("m_projectPath", &ApplicationInfo::m_projectPath)
+		.def_readwrite("m_applicationName", &ApplicationInfo::m_applicationName)
+		.def_readwrite("m_enableDocking", &ApplicationInfo::m_enableDocking)
+		.def_readwrite("m_enableViewport", &ApplicationInfo::m_enableViewport)
+		.def_readwrite("m_fullScreen", &ApplicationInfo::m_fullScreen);
 
 	py::class_<Application>(m, "Application")
 		.def_static("initialize", &Application::Initialize)
