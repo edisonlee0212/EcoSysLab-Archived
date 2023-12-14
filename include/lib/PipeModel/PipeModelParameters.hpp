db0cafecbb3ffc7ed37cac59d323cf985dc52c75
Bosheng Li <li2343@purdue.edu>
2023-12-13 21:43:45 -0500

    Update PipeModelParameters.hpp

--- include/lib/PipeModel/PipeModelParameters.hpp
+++ include/lib/PipeModel/PipeModelParameters.hpp
@@ -11,7 +11,7 @@ namespace EcoSysLab
 		int m_maxSimulationIterationCellFactor = 100;
 		int m_timeout = 10000;
 		int m_stabilizationCheckIteration = 200;
-		float m_stabilizationMovementDistance = 0.1f;
+		float m_stabilizationMovementDistance = 0.05f;
 		float m_splitRatioLimit = 0.5f;
 		int m_endNodeStrands = 3;
 	};
