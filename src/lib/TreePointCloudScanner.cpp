71a3d5910a3db63c2eadebe41c229915d3380dda
Bosheng Li <li2343@purdue.edu>
2024-01-03 10:59:46 -0500

    Update TreePointCloudScanner.cpp

--- src/lib/TreePointCloudScanner.cpp
+++ src/lib/TreePointCloudScanner.cpp
@@ -12,6 +12,15 @@ using namespace tinyply;
 using namespace EcoSysLab;
 
 #ifndef BUILD_WITH_RAYTRACER
+struct HitInfo {
+	glm::vec3 m_position = glm::vec3(0.0f);
+	glm::vec3 m_normal = glm::vec3(0.0f);
+	glm::vec3 m_tangent = glm::vec3(0.0f);
+	glm::vec4 m_color = glm::vec4(1.0f);
+	glm::vec2 m_texCoord = glm::vec2(0.0f);
+	glm::vec3 m_data = glm::vec4(0.0f);
+	glm::vec2 m_data2 = glm::vec4(0.0f);
+};
 struct PointCloudSample {
 	// Input
 	glm::vec3 m_direction = glm::vec3(0.0f);
